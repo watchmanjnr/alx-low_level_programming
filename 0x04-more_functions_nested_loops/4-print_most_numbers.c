098530bd203b55f452323460dbd7792cb75df1f1
watchmanjnr <benjamin.djangmah@gmail.com>
2022-12-15 10:48:25 -0800

    4-print_most_numbers new

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -8,12 +9,15 @@
 
 void print_most_numbers(void)
 {
-	char c;
+int n;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-		if (!(c == '2' || c == '4'))
-			_putchar(c);
-	}
-	_putchar('\n');
+for (n = 48; n < 58; n++)
+{
+if ((n == 50) || (n == 52))
+{
+continue;
+}
+putchar(n);
+}
+putchar('\n');
 }
