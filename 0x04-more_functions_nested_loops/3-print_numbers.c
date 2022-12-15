85b0b3fd5459a89e86750f9b90f66f129d7cafe5
watchmanjnr <benjamin.djangmah@gmail.com>
2022-12-15 10:22:20 -0800

    3-print_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,15 @@
 
 /**
  * print_numbers - print numbers from 0-9
- *Return: void
  */
 
 void print_numbers(void)
 {
-	char c;
+	int n;
 
-	for (c = '0'; c <= '9'; c++)
+	for (n = 0; n <= 9; n++)
 	{
-		_putchar(c);
+	_putchar((n % 10) + '0');
 	}
 	_putchar ('\n');
 }
