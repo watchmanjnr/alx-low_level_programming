a15fb6f684e6a2cdd673f5e4e3de1cfdd308c576
watchmanjnr <benjamin.djangmah@gmail.com>
2022-12-15 09:58:51 -0800

    3-print_numbers correction

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - print numbers from 0-9
- * Return: void
+ *Return: void
  */
 
 void print_numbers(void)
@@ -11,9 +11,7 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(c)
+		_putchar(c);
 	}
 	_putchar ('\n');
-
-	return (void);
 }
