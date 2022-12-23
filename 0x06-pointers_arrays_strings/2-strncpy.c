718af4c16d5cca4481ce413260024e3c5c0bc04e
watchmanjnr <benjamin.djangmah@gmail.com>
2022-12-22 21:12:38 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,15 +9,15 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-int i
+	int i;
 
-	for (i = 0; i < n && *(src + i); i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		*(dest + i) = *(src + i);
+		dest[i] = src[i];
 	}
 	for (; i < n; i++)
 	{
-	*(dest + i) = '\0';
+		dest[i] = '\0';
 	}
 	return (dest);
 }
