f1447fcb034cada4d1257e1ca674b18029870d0f
watchmanjnr <benjamin.djangmah@gmail.com>
2023-01-03 19:13:29 -0800

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strstr - function that locates a substring.
+ * @haystack: string to locate
+ * @needle: substring to locate
+ * Return: returns string at starting sub or null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	if (needle[0] == '\0')
+		return (haystack);
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[1] == needed[0])
+		{
+
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+					break;
+			}
+			if (needle[j] == '\0')
+				return (haystack + i);
+		}
+	}
+	return ('\0');
+}
