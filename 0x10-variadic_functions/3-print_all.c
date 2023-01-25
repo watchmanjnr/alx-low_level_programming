05ffb2efd24c19f2d3cc0512722c5665a729ca4a
watchmanjnr <benjamin.djangmah@gmail.com>
2023-01-24 19:47:46 -0800

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,7 +3,6 @@
 /**
  * print_all - Prints all of the arguments when specified
  * @format: specifies the necessary operations
- *
  * Return: void
  */
 
@@ -20,13 +19,13 @@ void print_all(const char * const format, ...)
 	{
 		switch (format[i])
 		{
-			case 'c';
+			case 'c':
 				printf("%c", va_arg(a_list, int));
 				flag = 0;
 				break;
 			case 'i':
 				printf("%i", va_arg(a_list, int));
-				flag = 0
+				flag = 0;
 				break;
 			case 'f':
 				printf("%f", va_arg(a_list, double));
