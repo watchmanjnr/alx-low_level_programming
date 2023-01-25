8acf1ee4e2ea9260cd4d71a7b14fc7cc9bb68d23
watchmanjnr <benjamin.djangmah@gmail.com>
2023-01-24 20:21:46 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,18 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_numbers - print numbers from 0-9
- */
+*print_numbers - print 0123456789
+* owned by Bwave ICT
+*Return: void
+*/
 
 void print_numbers(void)
 {
-	int n;
+	char c;
 
-	for (n = '0'; n <= '9'; n++)
-	_putchar((n % 10) + '0');
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
 	_putchar('\n');
 }
